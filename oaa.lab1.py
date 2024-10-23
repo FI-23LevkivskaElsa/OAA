ff4b16a09b90da37c52eae0d8e65536c121925a0
Elsa <164555677+FI-23LevkivskaElsa@users.noreply.github.com>
2024-10-23 12:43:06 +0300

    Update oaa.lab1.py

--- oaa.lab1.py
+++ oaa.lab1.py
@@ -14,8 +14,7 @@ class Table:
             try:
                 int(value)
             except ValueError:
-                return f"Помилка: значення '{value}' не є  цілим числом"
-
+                return f"Помилка: значення '{value}' не є цілим числом"
         self.rows.append([int(value) for value in values])
         return f"1 рядок додано до таблиці {self.name}."
 
@@ -28,15 +27,15 @@ class Table:
             for i, value in enumerate(row):
                 column_widths[i] = max(column_widths[i], len(str(value)))
 
-        header = " | ".join([col['назва'].ljust(column_widths[i]) for i, col in enumerate(self.columns)])
-        separator = "-" * len(header)
+        header = "| " + " | ".join([col['назва'].ljust(column_widths[i]) for i, col in enumerate(self.columns)]) + " |"
+        separator = "+" + "-" * (len(header) - 2) + "+"
 
         formatted_rows = []
         for row in self.rows:
-            formatted_row = " | ".join([str(value).ljust(column_widths[i]) for i, value in enumerate(row)])
+            formatted_row = "| " + " | ".join([str(value).ljust(column_widths[i]) for i, value in enumerate(row)]) + " |"
             formatted_rows.append(formatted_row)
 
-        table_str = f"{header}\n{separator}\n" + "\n".join(formatted_rows)
+        table_str = f"{separator}\n{header}\n{separator}\n" + "\n".join(formatted_rows) + f"\n{separator}"
         return table_str if formatted_rows else "Таблиця порожня."
 
     @classmethod
@@ -52,13 +51,13 @@ class Table:
             table.add_column(column_input, indexed)
         print(table)
         while True:
-            insert_input = input("Введіть дані для вставки через кому (або напишіть 'стоп', щоб завершити): ")
+            insert_input = input("Введіть дані для вставки через КОМУ (або напишіть 'стоп', щоб завершити): ")
             if insert_input.lower() == 'стоп':
                 break
             values = [val.strip() for val in insert_input.split(",")]
             result = table.insert(values)
             print(result)
-        print("\nТаблиця після вставки даних:")
+        print(f"\nТаблиця {table_name} після вставки даних має вигляд:")
         print(table.display())
 
 Table.create_table_interactive()
